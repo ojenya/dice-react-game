709a88dc8d23b85c47456f0bc9dda942a218bf61
Evgenii Oseledets <evgenii.oseledets@ax-team.com>
2023-11-12 13:33:05 +0300

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="card">
+        <Button onClick={() => setCount((count) => count + 1)} variant="secondary">
+          count is {count}
+        </Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
